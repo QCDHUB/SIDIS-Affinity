1f21e468c808dbbe02d7275b10788df6479735a7
Scott <Juniper82>
2021-12-01 22:20:06 -0500

    changed font within plottools.py

--- plottools.py
+++ plottools.py
@@ -6,6 +6,9 @@ from scipy.spatial import ConvexHull, convex_hull_plot_2d
 import pylab as py
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+# import matplotlib as mpl
+
+# mpl.rcParams['font.serif'] = ['Times New Roman']
 
 
 def custom_label(label):
@@ -1399,7 +1402,9 @@ def plotEIC(fname, hadron = 'pi+', affinity = 'tmdaff', plotx = 'qT', ploty = 'z
 
 
 def plotEIC1(data, predictions, hadron = 'pi+', affinity = 'tmdaff', plotx = 'qT', ploty = 'z', cmap_name = 'seismic_r', yscale = 'linear'):
-
+    plt.rc('font',**{'family':'serif','serif':['Times New Roman']})
+    plt.rc('text', usetex=True)
+   # plt.rc('font', family='serif')
     
     data['Q']=data['Q2']**0.5
     if 'had' not in data.keys() and 'hadron' in data.keys(): 
